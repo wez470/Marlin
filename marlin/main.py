68c488a5899ef01f6940b033cfd4d0abcab51fcf
Weston Carlson <wez470@gmail.com>
2020-11-14 18:03:19 -0600

    Add play button.

--- marlin/main.py
+++ marlin/main.py
@@ -17,6 +17,7 @@ class FileLoader(BoxLayout):
 class Root(GridLayout):
     label = ObjectProperty(None)
     wpm_text = ObjectProperty(None)
+    play_button = ObjectProperty(None)
 
     def __init__(self, *args, **kwargs):
         super(Root, self).__init__(*args, **kwargs)
@@ -24,6 +25,7 @@ class Root(GridLayout):
         self.text_event = None
         self.text = None
         self.curr_word = 0
+        self.playing = False
         self.wpm_text.bind(text=self.wpm_updated)
 
     def dismiss_popup(self):
@@ -44,6 +46,13 @@ class Root(GridLayout):
                 self.update_timer()
         self.dismiss_popup()
 
+    def toggle_play(self):
+        if self.text and self.curr_word >= len(self.text) - 1:
+            self.curr_word = 0
+        self.playing = not self.playing
+        self.play_button.text = "Pause" if self.playing else "Play"
+        self.update_timer()
+
     def wpm_updated(self, _, text):
         if text:
             self.wpm = int(text)
@@ -52,17 +61,18 @@ class Root(GridLayout):
     def update_timer(self):
         if self.text_event is not None:
             self.text_event.cancel()
-        if self.wpm > 0:
+        if self.wpm > 0 and self.playing:
             self.text_event = Clock.schedule_interval(lambda dt: self.update_text(), 60.0 / self.wpm)
 
     def update_text(self):
         if self.text is not None:
-            if self.curr_word >= len(self.text) - 1:
-                self.text_event.cancel()
+            if self.curr_word >= len(self.text):
+                self.toggle_play()
                 return
-            self.curr_word += 1
             self.label.text = self.text[self.curr_word]
-
+            self.curr_word += 1
+        else:
+            self.toggle_play()
 
 # Create the App class
 class MarlinApp(App):
